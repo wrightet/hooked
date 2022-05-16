1460a4151a7073739d0268255d25c60263609195
Ethan Wright <wrightet707@gmail.com>
2022-05-16 14:31:34 -0700

    start nav bar

---
+++ hooked-frontend/src/components/navbar.js
@@ -0,0 +1,16 @@
+function NavBar(){
+    return(
+        <div>
+            <ul>
+                <li>
+                    test
+                </li>
+                <li>
+                    test
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
